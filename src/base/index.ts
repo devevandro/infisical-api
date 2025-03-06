dc6689d8c591157bb4b4a70a31b398066112d588
Evandro Carvalho Ferreira <dev.evandro@gmail.com>
2025-03-05 23:28:37 -0300

    Update index.ts

--- src/base/index.ts
+++ src/base/index.ts
@@ -11,7 +11,7 @@ export class InfisicalBaseApi {
       baseURL: `${INFISICAL_API_URL}/v1`,
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eUlkIjoiYTMzNjExODgtZWQ3Zi00YmMxLTk3ZjUtZDNiODBlYjFkY2I3IiwiaWRlbnRpdHlBY2Nlc3NUb2tlbklkIjoiMDZkNDIyNGYtMDBmMC00MzBjLTliZjQtZjRhZmQ3ZjdhNzQ0IiwiYXV0aFRva2VuVHlwZSI6ImlkZW50aXR5QWNjZXNzVG9rZW4iLCJpYXQiOjE3NDEyMjA2MjgsImV4cCI6MTc0MzgxMjYyOH0.UmfJYczxzaa_YkyL6_phYK3Q7q9A8wlA7pp8UcfITDE`,
+        Authorization: `Bearer `,
       },
     });
   }
